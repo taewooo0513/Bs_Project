8f8323c53689f5c9c0d664c20243ba87bc209aaa
taewoong0513 <taewooo0513@gmail.com>
2020-09-03 16:57:10 +0900

    뭘작업했는지

---
+++ Assets/Script/BackGround/BackGoundCome.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGoundCome : MonoBehaviour
+{
+    public Transform Target;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Target.position.x, Target.position.y, 10);
+    }
+}
---
+++ Assets/Script/Player/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    private float h;
+    private float v;
+    private Animator anime;
+    public SpriteRenderer spr;
+    // Update is called once per frame
+    private void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();
+        anime = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        v = Input.GetAxis("Vertical")*speed *Time.deltaTime;
+        transform.Translate(h,v,0);
+        anime.SetFloat("MoveX", Input.GetAxis("Horizontal"));
+        anime.SetFloat("MoveY", Input.GetAxis("Vertical"));
+
+      if(h >= 0.0001)
+        {
+            spr.flipX = false;
+        }
+       else if (h <= -0.0001)
+        {
+            spr.flipX = true;
+        }
+    }
+}
